ffb411408f66aef5c6ae0de3e072c3ffc783d0aa
Кирилл Якунченков <kirill.yakunchenkov@gmail.com>
2025-10-10 04:01:09 +0300

    Созданы коды ошибок, записаны в constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,6 @@
-SAMPLE_CONSTANT: int = 10
+# Коды ошибок
+ERROR_INVALID_TOKEN = "E1: Недопустимый токен"
+ERROR_DIVISION_BY_ZERO = "E2: Зафиксирована попытка деления на ноль, штраф 5000 руб"
+ERROR_UNEXPECTED_END = "E3: Неожиданный конец выражения"
+ERROR_MISMATCHED_PARENTHESIS = "E4: Несогласованные скобки"
+ERROR_INVALID_NUMBER = "E5: Некорректное число"
