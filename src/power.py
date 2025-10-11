42e98df52510e52daac71e5d669980a969646351
Кирилл Якунченков <kirill.yakunchenkov@gmail.com>
2025-10-11 22:59:06 +0300

    Изменен токенизатор и парсер, добавлен pytest

--- src/power.py
+++ src/power.py
@@ -1,167 +1,127 @@
 import re
-from typing import List, Union
-from src import constants
+from .constants import (
+    ERROR_INVALID_TOKEN,
+    ERROR_DIVISION_BY_ZERO,
+    ERROR_UNEXPECTED_END,
+    ERROR_MISMATCHED_PARENTHESIS,
+    ERROR_INVALID_NUMBER
+)
 
-Token = Union[float, str]
-
-def is_integer_value(x):
-    return isinstance(x, int) or (isinstance(x, float) and x.is_integer())
-
-def tokenize(expr: str) -> List[Token]:
-    # Убираем пробелы (или можно оставить \s+ в регулярке)
+def tokenize(expr: str) -> list:
+    """Разбивает строку на токены: числа, операторы, скобки."""
     expr = expr.replace(' ', '')
     if not expr:
-        raise ValueError(constants.ERROR_INVALID_TOKEN)
+        raise ValueError(ERROR_INVALID_TOKEN)
 
-    # ВАЖНО: сначала длинные операторы (**, //), потом короткие (+, -, *, /, %, (, ))
-    token_pattern = r'(\d+\.?\d*|\.\d+|\*\*|//|[+\-*/%()])'
-    tokens = re.findall(token_pattern, expr)
+    tokens = re.findall(r'\d+\.?\d*|\.\d+|\*\*|[+\-*/%()]', expr)
 
-    # Проверка: всё ли распозналось?
-    if not tokens or ''.join(tokens) != expr:
-        raise ValueError(constants.ERROR_INVALID_TOKEN)
+    if ''.join(tokens) != expr:
+        raise ValueError(ERROR_INVALID_TOKEN)
 
     result = []
-    i = 0
-    while i < len(tokens):
-        token = tokens[i]
-        # Обработка унарного +/- 
-        if token in '+-' and (i == 0 or tokens[i - 1] in '(+-*/%'):
-            if i + 1 >= len(tokens):
-                raise ValueError(constants.ERROR_UNEXPECTED_END)
-            next_token = tokens[i + 1]
-            if next_token not in ('+', '-', '('):
-                try:
-                    num = float(next_token)
-                    if token == '-':
-                        num = -num
-                    result.append(num)
-                    i += 2
-                except ValueError:
-                    raise ValueError(constants.ERROR_INVALID_NUMBER)
-            
-            else:
-                result.append(token)
-                i += 1
-
-        elif token == '**':
-            result.append('**')
-            i += 1
-        elif token == '//':
-            result.append('//')
-            i += 1
-        elif token in '()':
+    for token in tokens:
+        if token in ('**', '+', '-', '*', '/', '//', '%', '(', ')'):
             result.append(token)
-            i += 1
-        elif token in '+-*/%':
-            result.append(token)
-            i += 1
         else:
-            # Число
             try:
-                if '.' in token or 'e' in token.lower():
+                if '.' in token:
                     result.append(float(token))
                 else:
                     result.append(int(token))
-                i += 1
             except ValueError:
-                raise ValueError(constants.ERROR_INVALID_NUMBER)
+                raise ValueError(ERROR_INVALID_NUMBER)
     return result
 
-class Parser:
-    def __init__(self, tokens: List[Token]):
-        self.tokens = tokens
-        self.pos = 0
 
-    def peek(self) -> Token:
-        if self.pos >= len(self.tokens):
-            raise ValueError(constants.ERROR_UNEXPECTED_END)
-        return self.tokens[self.pos]
+def evaluate(expression: str) -> float:
+    """Вычисляет арифметическое выражение со скобками и приоритетами."""
+    tokens = tokenize(expression)
+    pos = 0
 
-    def consume(self) -> Token:
-        token = self.peek()
-        self.pos += 1
+    def peek():
+        nonlocal pos
+        if pos >= len(tokens):
+            raise ValueError(ERROR_UNEXPECTED_END)
+        return tokens[pos]
+
+    def consume():
+        nonlocal pos
+        token = peek()
+        pos += 1
         return token
 
-    def parse(self) -> float:
-        result = self.expr()
-        if self.pos != len(self.tokens):
-            raise ValueError(constants.ERROR_INVALID_TOKEN)
-        return result
+    def primary() -> float:
+        nonlocal pos
+        token = consume()
+        if isinstance(token, (int, float)):
+            return float(token)
+        elif token == '(':
+            result = expr()
+            if pos >= len(tokens) or consume() != ')':
+                raise ValueError(ERROR_MISMATCHED_PARENTHESIS)
+            return result
+        else:
+            raise ValueError(ERROR_INVALID_TOKEN)
 
-    def expr(self) -> float:
-        return self.add()
+    def unary() -> float:
+        nonlocal pos
+        if pos < len(tokens) and peek() in ('+', '-'):
+            op = consume()
+            value = unary()
+            return value if op == '+' else -value
+        else:
+            return primary()
 
-    def add(self) -> float:
-        left = self.mul()
-        while self.pos < len(self.tokens) and self.peek() in ('+', '-'):
-            op = self.consume()
-            right = self.mul()
+    def pow_() -> float:
+        left = unary()
+        if pos < len(tokens) and peek() == '**':
+            consume()
+            right = pow_()
+            return left ** right
+        return left
+
+    def mul() -> float:
+        left = pow_()
+        while pos < len(tokens) and peek() in ('*', '/', '//', '%'):
+            op = consume()
+            right = pow_()
+            if op == '*':
+                left *= right
+            elif op == '/':
+                if right == 0:
+                    raise ValueError(ERROR_DIVISION_BY_ZERO)
+                left /= right
+            elif op == '//':
+                if not (isinstance(left, int) and isinstance(right, int)):
+                    raise ValueError("Оператор // допустим только для целых чисел")
+                if right == 0:
+                    raise ValueError(ERROR_DIVISION_BY_ZERO)
+                left //= right
+            elif op == '%':
+                if not (isinstance(left, int) and isinstance(right, int)):
+                    raise ValueError("Оператор % допустим только для целых чисел")
+                if right == 0:
+                    raise ValueError(ERROR_DIVISION_BY_ZERO)
+                left %= right
+        return left
+
+    def add() -> float:
+        left = mul()
+        while pos < len(tokens) and peek() in ('+', '-'):
+            op = consume()
+            right = mul()
             if op == '+':
                 left += right
             else:
                 left -= right
         return left
 
-    def mul(self) -> float:
-        left = self.pow()
-        while self.pos < len(self.tokens) and self.peek() in ('*', '/', '//', '%'):
-            op = self.consume()
-            right = self.pow()
-            if op == '*':
-                left *= right
-            elif op == '/':
-                if right == 0:
-                    raise ValueError(constants.ERROR_DIVISION_BY_ZERO)
-                left /= right
-            elif op == '//':
-                if not (is_integer_value(left) and is_integer_value(right)):
-                    raise ValueError("Деление без остатка допустимо только для целых чисел")
-                if right == 0:
-                    raise ValueError(constants.ERROR_DIVISION_BY_ZERO)
-                left = int(left)
-                right = int(right)
-                left //= right
-            elif op == '%':
-                if not (is_integer_value(left) and is_integer_value(right)):
-                    raise ValueError("Нахождение остатка допустимо только для целых чисел")
-                if right == 0:
-                    raise ValueError(constants.ERROR_DIVISION_BY_ZERO)
-                left = int(left)
-                right = int(right)
-                left %= right
-        return left
+    def expr() -> float:
+        return add()
 
-    def pow(self) -> float:
-        left = self.unary()
-        if self.pos < len(self.tokens) and self.peek() == '**':
-            self.consume()
-            right = self.pow() #Возведение в степеь - правоассоциативная операция
-            return left ** right
-        return left
+    result = expr()
 
-    def unary(self) -> float:
-        if self.pos < len(self.tokens) and self.peek() in ('+', '-'):
-            op = self.consume()
-            value = self.unary()
-            return value if op == '+' else -value
-        else:
-            return self.primary()
+    if pos != len(tokens):
+        raise ValueError(ERROR_INVALID_TOKEN)
 
-    def primary(self) -> float:
-        token = self.consume()
-        if isinstance(token, (int, float)):
-            return float(token)
-        elif token == '(':
-            result = self.expr()
-            if self.pos >= len(self.tokens) or self.consume() != ')':
-                raise ValueError(constants.ERROR_MISMATCHED_PARENTHESIS)
-            return result
-        else:
-            raise ValueError(constants.ERROR_INVALID_TOKEN)
-
-
-def evaluate(expression: str) -> float:
-    tokens = tokenize(expression)
-    parser = Parser(tokens)
-    return parser.parse()
+    return result
